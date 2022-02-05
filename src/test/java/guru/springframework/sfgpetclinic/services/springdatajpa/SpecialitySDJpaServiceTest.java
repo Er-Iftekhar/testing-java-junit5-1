0205506118d62c5ccc58a1cb427b474f4bb264e0
SyedIftekharuddin <syedy636@gmail.com>
2022-02-05 22:25:58 +0100

    will continue in the same branch

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,5 +1,36 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
 
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    SpecialitySDJpaService specialitySDJpaService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testDeleteById() {
+        specialitySDJpaService.deleteById(  1L);
+    }
+
+    @Test
+    void testDelete() {
+        specialitySDJpaService.delete(new Speciality());
+    }
 }
